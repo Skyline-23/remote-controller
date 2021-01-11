294d794433e9a5f6e14854a9b258a37ade256530
apple19760401 <bs2740@naver.com>
2021-01-11 17:01:20 +0900

    Initial Commit

---
+++ remote-controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remote-controller
+//
+//  Created by 김부성 on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
